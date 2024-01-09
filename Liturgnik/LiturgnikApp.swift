47d02e09c228bb496230e134485350b3e5e93d5c
Olivier Marszałkowski <marszalkowskiolivier@gmail.com>
2024-01-09 10:35:46 +0100

    Initial Commit

---
+++ Liturgnik/LiturgnikApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiturgnikApp.swift
+//  Liturgnik
+//
+//  Created by Olivier Marszałkowski on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LiturgnikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
