eddc35b4e29a77afd4c17d5b9db387ec68224b79
Olivier Marszałkowski <marszalkowskiolivier@gmail.com>
2024-02-06 17:29:56 +0100

    powiekszanie i pomnijeszanie tekstu

---
+++ Liturgnik/Components/InternetConnectionErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  InternetConnectionErrorView.swift
+//  Liturgnik
+//
+//  Created by Olivier Marszałkowski on 05/02/2024.
+//
+
+import SwiftUI
+
+struct InternetConnectionErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InternetConnectionErrorView()
+}
